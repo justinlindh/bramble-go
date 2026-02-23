2bb1ca52301470b4f4ec3a9b8a2bdf51cfdc7842
Justin Lindh <justinlindh@gmail.com>
2026-02-22 23:18:07 -0800

    chore(version): bump sdk to 0.3.0 for protocol 0.3.0

--- version.go
+++ version.go
@@ -6,10 +6,10 @@ const (
 	MinProtocolVersion = "0.1.0"
 
 	// MaxProtocolVersion is the maximum protocol version this SDK can speak.
-	MaxProtocolVersion = "0.2.1"
+	MaxProtocolVersion = "0.3.0"
 
 	// SDKVersion is the version of this SDK itself.
-	SDKVersion = "0.2.1"
+	SDKVersion = "0.3.0"
 )
 
 // IsCompatible reports whether the given firmware protocol version string
